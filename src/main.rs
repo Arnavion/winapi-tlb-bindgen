b05a1126c9ccdff80fa72e211483ce9baa870242
Arnavion <arnavion@gmail.com>
2017-03-26 18:48:02 -0700

    Emit empty structs instead of unit structs for coclasses so that they don't create problems with shadowing.

--- src/main.rs
+++ src/main.rs
@@ -253,7 +253,7 @@ quick_main!(|| -> ::error::Result<()> {
 
 				::winapi::um::oaidl::TKIND_COCLASS => {
 					println!("#[repr(C)]");
-					println!("pub struct {};", type_name);
+					println!("pub struct {} {{}}", type_name);
 					println!();
 					println!("impl {} {{", type_name);
 					println!("    #[inline]");
